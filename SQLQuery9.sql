3e943a86a78d472e6ef53656f8aaf3eade1a0eb6
rkt121416 <91348485+rkt121416@users.noreply.github.com>
2023-01-20 11:55:45 -0500

    Delete SQLQuery9.sql

--- SQLQuery9.sql
+++
@@ -1,5 +0,0 @@
-SELECT SUM(new_cases) AS total_cases, SUM(cast(new_deaths as int)) AS total_deaths, SUM(cast(new_deaths AS int))/SUM(new_cases)*100 AS death_percentage
-FROM dbo.covid_deaths
-WHERE continent is not null
-order by 1,2
-
